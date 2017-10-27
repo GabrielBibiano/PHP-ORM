888befd2801f449e207fffeb495b743b45c7a7ec
GabrielBibiano <bibiano@tijucaalimentos.com>
2017-10-27 14:08:26 -0300

    Primeiros passos

---
+++ Erp.class.php
@@ -0,0 +1,26 @@
+<?php
+    namespace Erp;
+    use PDO;
+
+    class Erp {
+        
+    }
+
+    class Database {
+        public function conn ( $host, $database, $user, $pass, $callback = '' ) {
+            try {
+                $dbh = new PDO( "mysql:host=$host;dbname=$database", $user, $pass );
+                if( is_callable( $callback ) !== true ) {
+                    if( $callback !== '' ){
+                        throw new Exception( "O callback não é uma função" ); 
+                    }
+                } else {
+                    return $callback();
+                }
+            } catch ( PDOException $e ) {
+                print "Error!: " . $e->getMessage();
+                die();
+            }
+        }
+    }
+?>
---
+++ autoload.php
@@ -0,0 +1,6 @@
+<?php
+    include "Erp.class.php";
+    include "DB.class.php";
+    use Erp;
+    $DB = new Erp\Database();
+?>
